70424ccbd585fc02efe2663a2ab9a085aed2b4d2
Steven Li <steven.li@seneceesg.com>
2021-07-06 17:40:33 +0800

    Updates.

--- script.sh
+++ script.sh
@@ -10,11 +10,25 @@ if [[ -z $INPUT_KEY ]]; then
     exit -1
 fi
 
-host="https://api.day.app"
-request_url=$host/$INPUT_KEY/
-request_body="title=${INPUT_TITLE}&body=${INPUT_BODY}&sound=${INPUT_SOUND}&isArchive=${INPUT_ISARCHIVE}&url=${INPUT_URL}&automaticallyCopy=${INPUT_AUTOMATICALLYCOPY}&copy=${INPUT_COPY}"
+request_url=$INPUT_HOST/$INPUT_KEY/
+request_body="title=${INPUT_TITLE}&body=${INPUT_BODY}&sound=${INPUT_SOUND}"
 
-echo -e "${cyan}Request url${none}: ${request_url}"
+if [[ -z $INPUT_ISARCHIVE ]]; then
+    request_body="${request_body}&isArchive=${INPUT_ISARCHIVE}"
+fi
+
+if [[ -z $INPUT_URL ]]; then
+    request_body="${request_body}&url=${INPUT_URL}"
+fi
+
+if [[ -z $INPUT_AUTOMATICALLYCOPY ]]; then
+    request_body="${request_body}&automaticallyCopy=${INPUT_AUTOMATICALLYCOPY}"
+fi
+if [[ -z $INPUT_COPY ]]; then
+    request_body="${request_body}&copy=${INPUT_COPY}"
+fi
+
+echo -e "${cyan}Request host${none}: ${request_url}"
 echo -e "${cyan}Request body${none}:\n${request_body//&/\\n}"
 
 res=`curl -s -X POST -d "${request_body}" ${request_url}`

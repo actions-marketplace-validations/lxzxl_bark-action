0868035fe8820a3e3fd919937cfc41dc1d85db61
Steven Li <steven.li@seneceesg.com>
2021-07-06 18:27:05 +0800

    feat: use v2 api.

--- script.sh
+++ script.sh
@@ -10,26 +10,17 @@ if [[ -z $INPUT_KEY ]]; then
     exit -1
 fi
 
-request_url=$INPUT_HOST/$INPUT_KEY/
-request_body="title=${INPUT_TITLE}&body=${INPUT_BODY}&sound=${INPUT_SOUND}"
+res=`curl -X "POST" "https://bark.etcd.run/push" \
+     -H 'Content-Type: application/json; charset=utf-8' \
+     -d $'{
+  "device_key": "${INPUT_KEY}",
+  "title": "${INPUT_TITLE}",
+  "body": "${INPUT_BODY}",
+  "ext_params": {
+    "url": "${INPUT_URL}"
+  },
+  "category": "${INPUT_CATEGORY}",
+  "sound": "${INPUT_SOUND}"
+}'`
 
-if [[ ! -z $INPUT_ISARCHIVE ]]; then
-    request_body="${request_body}&isArchive=${INPUT_ISARCHIVE}"
-fi
-
-if [[ ! -z $INPUT_URL ]]; then
-    request_body="${request_body}&url=${INPUT_URL}"
-fi
-
-if [[ ! -z $INPUT_AUTOMATICALLYCOPY ]]; then
-    request_body="${request_body}&automaticallyCopy=${INPUT_AUTOMATICALLYCOPY}"
-fi
-if [[ ! -z $INPUT_COPY ]]; then
-    request_body="${request_body}&copy=${INPUT_COPY}"
-fi
-
-echo -e "${cyan}Request host${none}: ${request_url}"
-echo -e "${cyan}Request body${none}:\n${request_body//&/\\n}"
-
-res=`curl -s -X POST -d "${request_body}" ${request_url}`
 echo -e "${green}Result${none}: ${res}"
